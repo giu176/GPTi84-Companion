8b27d81d2e9903459f23aa61b04e1baa8a69c512
Xander Pickering <xandwrp@gmail.com>
2026-04-28 05:38:21 -0600

    Man...

--- lean/p1100/Main.lean
+++
@@ -1,4 +0,0 @@
-import P1100
-
-def main : IO Unit :=
-  IO.println s!"Hello, {hello}!"
--- lean/p1100/P1100.lean
+++
@@ -1,3 +0,0 @@
--- This module serves as the root of the `P1100` library.
--- Import modules here that should be built as part of the library.
-import P1100.Basic
